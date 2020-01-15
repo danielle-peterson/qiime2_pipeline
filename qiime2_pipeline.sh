15d9554e552585c05d819508d1eb603f2534f4d4
Kevin Bonham, PhD <kevbonham@gmail.com>
2020-01-15 13:35:24 -0500

    first walkthrough w/danielle

--- qiime2_pipeline.sh
+++ qiime2_pipeline.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 # exit when any command fails
+# NOTE: Get conda env file
+# Test inputs:
+## C0047-7E-1A_S66_L001_R1_001.fastq.gz
+## C0047-7E-1A_S66_L001_R2_001.fastq.gz
+## C1009-1F-1A_S237_L001_R1_001.fastq.gz
+## C1009-1F-1A_S237_L001_R2_001.fastq.gz
+# Test Mapping file (1 row per sample w/metadata)
+## test_data_mapping.csv
+
 set -e
 
+# TODO: Don't hard code
 . /Users/danielle/bin/qiime2_config
 
-# echo "$project_name"
+echo "$project_name"
 mkdir qiime_outputs
 cd qiime_outputs
 
 mkdir reads_qza
-# qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
-#                    --input-path /Users/danielle/Documents/thesis/16S/qiime2/test_data \
-#                    --output-path reads_qza/reads.qza \
-#                    --input-format CasavaOneEightSingleLanePerSampleDirFmt
 
 # ultimately change this to echo "$data"
 ####### how to fix this ###########
+# NOTE: change hard-coded paths to inputs
 qiime tools import --type SampleData[PairedEndSequencesWithQuality] \
-                    --input-path /Users/danielle/Documents/thesis/16S/qiime2/test_data \
-                    --output-path reads_qza/reads.qza \
-                    --input-format CasavaOneEightSingleLanePerSampleDirFmt
+                   --input-path /Users/danielle/Documents/thesis/16S/qiime2/test_data \
+                   --output-path reads_qza/reads.qza \
+                   --input-format CasavaOneEightSingleLanePerSampleDirFmt
 
 qiime cutadapt trim-paired --i-demultiplexed-sequences reads_qza/reads.qza \
                     --p-cores 4 \
                     --p-front-f ^GTGYCAGCMGCCGCGGTAA \
                     --p-front-r ^CCGYCAATTYMTTTRAGTTT \
                     --o-trimmed-sequences reads_qza/reads_trimmed.qza
-
-# Take a subsample of paired end reads to run the rest of the pipeline.
-# Then, demultiplex these samples to allow us to connect the barcode
-# associated with each run to the sample that it came from
-
-# !mkdir reads_qza_subsample
-# from qiime2.plugins.demux.methods import subsample_paired
-# qiime demux subsample-paired --i-sequences reads_qza/reads_trimmed.qza\
-#                             --p-fraction 0.01\
-#                             --output-dir reads_qza_subsample
 
 # "Denoising" is the process of correcting or removing noisy reads
 # exports denoising_stats.qza, table.qza, representative_sequences.qza
@@ -48,7 +45,7 @@ qiime dada2 denoise-paired --i-demultiplexed-seqs reads_qza/reads_trimmed.qza \
 
 # convert stats QZA to tsv
 qiime tools export --input-path dada2_output/denoising_stats.qza \
-                --output-path dada2_output/denoising
+                   --output-path dada2_output/denoising
 
 
 qiime feature-table summarize --i-table dada2_output/table.qza --o-visualization dada2_output/table_summary.qzv
@@ -65,10 +62,13 @@ qiime feature-table filter-seqs --i-data dada2_output/representative_sequences.q
 qiime feature-table summarize --i-table dada2_output/table_filt.qza --o-visualization dada2_output/table_filt_summary.qzv
 
 qiime tools export --input-path dada2_output/table_filt_summary.qzv \
-                --output-path dada2_output/filt_summary
+                   --output-path dada2_output/filt_summary
 
 # finding p-max-depth to make rarefaction tree
+## Log
 head dada2_output/filt_summary/sample-frequency-detail.csv
+
+# TODO: Fix hard-coded pmax - max number of reads to use, take lowest from files?
 pmax=34917
 
 ########### Build quick phylogeny with FastTree
@@ -102,25 +102,22 @@ qiime phylogeny midpoint-root --i-tree tree_out/rep_seqs_filt_aligned_masked_tre
 # A key quality control step is to plot rarefaction curves for all of your samples to determine if you performed sufficient sequencing.
 # The below command will generate these plots (X is a placeholder for the maximum depth in your dataset).
 
-# set p-max-depth to variable
-
+# TODO: set p-max-depth to variable
 
 qiime diversity alpha-rarefaction --i-table dada2_output/table_filt.qza \
-                                --p-max-depth 34917 \
-                                --p-steps 20 \
-                                --i-phylogeny tree_out/rep_seqs_filt_aligned_masked_tree_rooted.qz.qza \
-                                --o-visualization rarefaction_curves_eachsample.qzv
+                                  --p-max-depth 34917 \
+                                  --p-steps 20 \
+                                  --i-phylogeny tree_out/rep_seqs_filt_aligned_masked_tree_rooted.qz.qza \
+                                  --o-visualization rarefaction_curves_eachsample.qzv
 
 # extract p-max-depth by extracting table_filt_summary.qzv
 
-# rarefaction curve with metadata
+# # TODO: Could decorate with metadata using `-m-metadata-file`
+# # rarefaction curve with metadata
 # qiime diversity alpha-rarefaction \
-#     --i-table dada2_output/table_filt.qza \
-#     --p-max-depth 12000  \
-#     --p-steps 20 \
-#     --i-phylogeny tree_out/rep_seqs_filt_aligned_masked_tree_rooted.qza \
-#     --m-metadata-file $mapping_file \
-#     --o-visualization rarefaction_curves.qzv
+#                ....
+#                --m-metadata-file $mapping_file \
+#                ....
 
 #### Assign taxonomy
 # You can assign taxonomy to your ASVs using a Naive-Bayes approach implemented
@@ -131,6 +128,8 @@ qiime diversity alpha-rarefaction --i-table dada2_output/table_filt.qza \
 # filename below is for the V6/V8 B969F-BA1406R region. The regions that we have
 # trained classifiers for are:
 
+# Using Pretrained classifier
+# TODO: Take out hard-coded path
 # add back classifier variable
 qiime feature-classifier classify-sklearn --i-reads dada2_output/rep_seqs_filt.qza \
                                           --i-classifier /Users/danielle/Documents/thesis/16S/qiime2/classifiers/silva-132-99-nb-classifier_pretrained.qza \
@@ -141,10 +140,10 @@ qiime tools export --input-path taxa/classification.qza --output-path taxa
 
 # A more useful output is the interactive stacked bar-charts of the taxonomic
 # abundances across samples, which can be output with this command:
-
+# TODO: Take out hard-coded path
 qiime taxa barplot --i-table dada2_output/table_filt.qza \
                    --i-taxonomy taxa/classification.qza \
-                    --o-visualization taxa/taxa_barplot.qzv\
+                   --o-visualization taxa/taxa_barplot.qzv\
                    --m-metadata-file /Users/danielle/Documents/thesis/16S/qiime2/mappingfiles/test_map.tsv
 
 # Calculating diversity metrics and generating ordination plots Common alpha and
@@ -155,10 +154,11 @@ qiime taxa barplot --i-table dada2_output/table_filt.qza \
 # a placeholder for the lowest reasonable sample depth; samples with depth below
 # this cut-off will be excluded).
 
+# TODO: Take out hard-coded path
 qiime diversity core-metrics-phylogenetic --i-table dada2_output/table_filt.qza \
                                           --i-phylogeny tree_out/rep_seqs_filt_aligned_masked_tree_rooted.qz.qza \
                                           --p-sampling-depth 34917 \
-                                         --m-metadata-file /Users/danielle/Documents/thesis/16S/qiime2/mappingfiles/test_map.tsv \
+                                          --m-metadata-file /Users/danielle/Documents/thesis/16S/qiime2/mappingfiles/test_map.tsv \
                                           --p-n-jobs 2 \
                                           --output-dir diversity
 
@@ -172,9 +172,9 @@ qiime composition add-pseudocount --i-table dada2_output/table_filt.qza \
                                   --o-composition-table dada2_output/table_filt_pseudocount.qza
 
 # qiime composition ancom --i-table dada2_output/table_filt_pseudocount.qza \
-#                       --m-metadata-file $mapping_file \
-#                       --m-metadata-column 'Method' \
-#                       --output-dir ancom_output
+#                         --m-metadata-file $mapping_file \
+#                         --m-metadata-column 'Method' \
+#                         --output-dir ancom_output
 
 # qiime tools export dada2_output/table_filt.qza --output-dir dada2_output_exported
 # qiime tools export dada2_output/rep_seqs_filt.qza --output-dir dada2_output_exported
